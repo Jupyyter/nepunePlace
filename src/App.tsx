f8d42cd02e28f516d7eeba59d928046d0120d0f3
) <raducea.matei2005@gmail.com>
2024-07-27 05:51:22 +0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Thing from './components/Thing';
 function App() {
-
+const words:string[]=["wow","cry of fear","cry of hunger"];
   return (
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <><h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Thing heading='list?' items={words} onSelectItem={(idk)=>{console.log(idk)}}>
+
+    </Thing>
+  
+  </>
   )
 }
 
