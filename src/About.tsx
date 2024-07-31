6d13ebb711f0c426d73f0d8d83de4de5576929c6
nepunePlace <raducea.matei2005@gmail.com>
2024-07-31 11:10:05 +0300

    aaaaa

--- src/About.tsx
+++ src/About.tsx
@@ -5,7 +5,7 @@ function About() {
       <main className="text-center p-4">
         <h1 className="text-4xl font-bold mb-4">about me? im not really interesting</h1>
         <p className="text-lg">
-        on the internet they use names derived from the names of the planets
+        on the internet i use names derived from the names of the planets
         </p>
       </main>
     </div>
