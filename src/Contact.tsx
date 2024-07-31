80a43fba8b02eead5543956617c883b9b5623d38
nepunePlace <raducea.matei2005@gmail.com>
2024-07-31 09:24:26 +0300

    aaaaa

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -3,9 +3,12 @@ function Contact() {
        return(
               <div className="flex-grow flex items-center justify-center">
       <main className="text-center p-4">
-        <h1 className="text-4xl font-bold mb-4">not yet</h1>
+        <h1 className="text-4xl font-bold mb-4">contacts</h1>
         <p className="text-lg">
-          not yet
+          my phone number: 0765823655
+        </p>
+        <p className="text-lg">
+          my email: raducea.matei2005@gmail.com
         </p>
       </main>
     </div>
