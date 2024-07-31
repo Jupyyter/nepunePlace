bee1af1dd2cd16e5b8963ee9aaf1b5153b3d30d2
nepunePlace <raducea.matei2005@gmail.com>
2024-07-31 11:51:18 +0300

    aaaaa

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,7 +1,6 @@
-
 function Contact() {
-       return(
-              <div className="flex-grow flex items-center justify-center">
+  return (
+    <div className="flex-grow flex items-center justify-center">
       <main className="text-center p-4">
         <h1 className="text-4xl font-bold mb-4">contacts</h1>
         <p className="text-lg">
@@ -10,9 +9,19 @@ function Contact() {
         <p className="text-lg">
           my email: raducea.matei2005@gmail.com
         </p>
+        <p className="text-lg">
+          <a 
+            href="https://github.com/Jupyyter?tab=repositories" 
+            className="text-yellow-400 hover:text-yellow-300 underline"
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            my github repositories
+          </a>
+        </p>
       </main>
     </div>
-       )
+  )
 }
 
 export default Contact;
